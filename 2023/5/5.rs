161eb059771ca8f8e65ad3438fc068fbaf3c9f49
Paul Colusso <paulcolusso@gmail.com>
2025-12-03 11:32:57 +1100

    ugh

--- 2023/5/5.rs
+++ 2023/5/5.rs
@@ -4,7 +4,6 @@
 //! ```cargo
 //! [dependencies]
 //! ```
-use ahash::AHashMap;
 
 fn main() {
 	//let filename = std::env::args().nth(1).expect("No filename provided");
@@ -17,61 +16,55 @@ fn main() {
 
     println!("seeds: {:?}", seeds);
 
-    const NUM_MAPS: usize = 7;
     const MAP_NAMES: [&'static str; 7] = ["Soil", "Fertilizer", "Water", "Light", "Temperature", "Humidity", "Location"];
-    //let mut maps: [AHashMap<u64, u64>; NUM_MAPS] = std::array::from_fn(|_| AHashMap::with_capacity(100000));
-    let mut maps: [Vec<(u64, u64)>; NUM_MAPS] = std::array::from_fn(|_| Vec::with_capacity(1_000_000));
-	let mut cursor = 0;
 
-	// skip empty line
-	lines.next();
+    let mut lowest = u64::MAX; // Tracks the lowest location, todo.
+    for seed in seeds {
+        let mut lines = input.lines();
+        // Skip seed lines
+        lines.next();
+        lines.next();
 
-	for l in lines {
-        if l == "" {
-            cursor += 1;
-            continue;
-        }
-        if l.contains("map") {
-            println!("map: {}", l);
-            continue;
-        } else {
+        let mut cursor = 0; // Tracks what map we're in
+        let mut current = seed; // Tracks what our current value is, after mapping
+        let mut mapped = false;
+
+        print!("Seed {seed}");
+
+        for l in lines {
+            if l == "" {
+                print!(" {} {}", MAP_NAMES[cursor], current);
+                cursor += 1;
+                mapped = false;
+                continue;
+            }
+            if l.contains("map") { continue; }
+            if mapped { continue; }
+
             let mut iter = l.split(" ");
             let dst = iter.next().expect("Map missing a dst").parse::<u64>().unwrap();
             let src = iter.next().expect("Map missing a src").parse::<u64>().unwrap();
             let rng = iter.next().expect("Map missing a rng").parse::<u64>().unwrap();
 
-            for i in 0..rng {
-                //maps[cursor].insert(src + i, dst + i);
-                maps[cursor].push((src + i, dst + i));
+            if current > src && current < src + rng {
+                current = dst + (current - src);
+                mapped = true;
             }
-        }
-    }
-    println!("Done parsing");
 
-    let mut lowest = u64::MAX;
-    for seed in seeds {
-        print!("Seed {seed}");
-
-        let mut current = seed;
-        for i in 0..NUM_MAPS {
-            let mut mapped = None;
-            for (k, v) in maps[i].iter() {
-                if current == *k {
-                    mapped = Some(v);
-                    break;
-                }
-            }
-            if let Some(v) = mapped {
-                current = *v;
-            }
-            //current = *maps[i].get(&current).unwrap_or(&current);
-            print!(" {} {}", MAP_NAMES[i], current);
+            // for i in 0..rng {
+            //     if src + i == current {
+            //         current = dst + i;
+            //         mapped = true;
+            //         break;
+            //     }
+            // }
         }
-        println!();
+
         if current < lowest {
             lowest = current;
         }
+        println!();
     }
 
-    println!("{}", lowest);
+    println!("Lowest location: {lowest}");
 }
